80a15552abd5b66297dce3a3044cd66936dc056c
Mark Miller <mmiller@vynyl.com>
2016-12-17 12:09:37 -0700

    added a net module

---
+++ app/src/main/java/com/brwskitime/dagger2/injection/NetModule.java
@@ -0,0 +1,50 @@
+package com.brwskitime.dagger2.injection;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by mark on 12/17/16.
+ */
+
+@Module
+public class NetModule {
+    String baseUrl;
+
+    public NetModule(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    Gson providesGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient() {
+        OkHttpClient client = new OkHttpClient();
+        return client;
+    }
+
+    Retrofit providesRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .build();
+        return retrofit;
+    }
+}
